30a9cd7153b69429dc1e4b31da704edd75c1807c
dmitriysokolov2023 <sokolovoff2023@gmail.com>
2025-02-02 23:58:38 +0300

    asfsdf

--- Python/python-example-main/mc/import pygame.py
+++ Python/python-example-main/mc/import pygame.py
@@ -1,41 +1,45 @@
-# Шаг 1
 import pygame
 import sys
 from random import randint
 
 pygame.init()
-pygame.mixer.init()  
-# Шаг 1
+pygame.mixer.init()
 
-
-# Шаг 2
 screen_width = 1000 
 screen_height = 800
 screen = pygame.display.set_mode((screen_width, screen_height))
-# Шаг 2
 
-# Шаг 5
 fon = pygame.image.load("stars-fon.jpg")
 ship_img = pygame.image.load("ship.png")
 fon_img = pygame.transform.scale(fon, (screen_width, screen_height))
 meteor_img = pygame.image.load("meteor.png")
 bom_img = pygame.image.load("bom.png")
-# Шаг 5
 
-# Шаг 6
+
+
+
+pygame.mixer.music.load("battle.mp3")
+pygame.mixer.music.play(-1)
+explosion_sound = pygame.mixer.Sound("bom.wav")
+
 shipX=screen_width/2-57.5
 shipY=screen_height - 200  
 
 meteorX = randint(int(shipX-200), int(shipX+200))
 meteorY = 0
 meteor_speed = 0.2
-# Шаг 6
 
-# Шаг 3
-pygame.display.set_caption("Space Score")
-# Шаг 3
 
-# Шаг 4
+
+
+
+FONT = pygame.font.Font(None, 48)
+def draw_text(text, color, x, y):
+    text_surface = FONT.render(text, True, color)
+    screen.blit(text_surface, (x, y))
+
+SCORE = 0
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -45,37 +49,45 @@ while True:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
-    # Шаг 8
-    meteorY +=meteor_speed
-    # Шаг 8
 
-    # Шаг 9
-    if(meteorY > screen_height):
-        meteor_speed +=0.1
-        meteorX = randint(int(shipX-150), int(shipX+150))
-        meteorY = 0
-    # Шаг 9
-
-    # Шаг 10
+    
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]: 
         shipX -= 2 
     if keys[pygame.K_RIGHT]:
         shipX += 2 
-    # Шаг 10
+    
 
-    # Шаг 11
     if shipX < 0:
         shipX = 0
     if shipX > screen_width-116:
         shipX = screen_width-116
-    # Шаг 11
-    
-    # Шаг 7
+
+    meteorY +=meteor_speed
     screen.blit(fon_img, (0,0))
     screen.blit(meteor_img, (meteorX,meteorY))
     screen.blit(ship_img, (shipX,shipY))
-    pygame.display.flip()
-    # Шаг 7
+    draw_text(f"Счёт: {SCORE}",(255,255,255),50,50)
+
+    if shipX < meteorX and shipX+115 > meteorX + 35 and shipY < meteorY:
+        screen.blit(fon_img, (0,0))
+        screen.blit(bom_img, (shipX,shipY))
+        draw_text(f"Счёт: {SCORE}",(255,255,255),50,50)
+        explosion_sound.play()
+        pygame.display.update()
+        pygame.time.delay(5000)
+        meteor_speed = 0.2
+        meteorY = 0
+        SCORE = 0
+        
+        
+
+    if(meteorY > screen_height):
+        meteor_speed +=0.1
+        meteorX = randint(int(shipX-150), int(shipX+150))
+        meteorY = 0
+        SCORE +=1
+
     
-# Шаг 4
+    pygame.display.flip()
+    
